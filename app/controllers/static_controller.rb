8aa0c604ea4cc1bb4b403f4a54479c76f784d1fc
Facundo Robert <robert.facundo@hotmail.com>
2025-07-25 15:51:08 -0300

    landing

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,5 +2,6 @@ class StaticController < ApplicationController
   skip_before_action :authenticate_request!, only: [:index]
 
   def index
+    render file: Rails.root.join('public', 'index.html'), layout: false, content_type: 'text/html'
   end
 end
